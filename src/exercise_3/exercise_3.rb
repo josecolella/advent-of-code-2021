af8e6dd1d5830a871e91f7ceaf1695d1d7eddb9e
Jose Colella <jose.colella@gusto.com>
2021-12-25 11:12:46 +0100

    feat(exercise-3): Finish solving part 2 of exercise 3

--- src/exercise_3/exercise_3.rb
+++ src/exercise_3/exercise_3.rb
@@ -1,102 +1,116 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
 
-diagnostic_report = []
-index = 0
-lines = []
-# O(n) -> n is the number of lines
-IO.foreach('./exercise_3_input.txt') do |line|
-  index = 0
-  clean_line = line.chomp
+module ExerciseThree
+  class BinaryDiagnostic
+    attr_reader :gamma_rate, :epsilon_rate, :oxygen_generator_rating, :scrubber_rating
 
-  lines << clean_line
-  # O(m) -> number of char in a line
-  clean_line.each_char do |char|
-    if diagnostic_report[index]
-      diagnostic_report[index][char] = 0 if diagnostic_report[index][char].nil?
-      diagnostic_report[index][char] += 1
-    else
-      count = {}
-      count[char] = 1
-      diagnostic_report << count
+    def initialize(file_string_path)
+      file = File.open(file_string_path)
+      @diagnostic_report = file.readlines.map(&:chomp)
+      file.close
     end
-    index += 1
-  end
-end
 
-gamma_rate = []
-epsilon_rate = []
+    def power_consumption
+      gamma_rate = []
+      epsilon_rate = []
 
-def get_diagnostic_report(lines)
-  d = []
-
-  lines.each do |line|
-    # O(m) -> number of char in a line
-    index = 0
-    line.each_char do |char|
-      if d[index]
-        d[index][char] = 0 if d[index][char].nil?
-        d[index][char] += 1
-      else
-        count = {}
-        count[char] = 1
-        d << count
+      number_distribution.each do |line|
+        gamma_rate << line.max_by { |_k, v| v }[0]
+        epsilon_rate << line.min_by { |_k, v| v }[0]
       end
-      index += 1
+
+      @gamma_rate = gamma_rate.join('').to_i(2)
+      @epsilon_rate = epsilon_rate.join('').to_i(2)
+
+      @gamma_rate * @epsilon_rate
+    end
+
+    def life_support_rating
+      input_line_length = @diagnostic_report.first.size
+      oxygen_rate = []
+      scrubber_rating = []
+
+      oxygen_rate_report = @diagnostic_report.dup
+      scrubber_rating_report = @diagnostic_report.dup
+
+      (0...input_line_length).each do |i|
+        oxygen_rate_distribution = number_distribution(oxygen_rate_report)
+        if oxygen_rate_distribution[i].count > 1
+          maximum = oxygen_rate_distribution[i].max_by { |_k, v| v }
+          minimum = oxygen_rate_distribution[i].min_by { |_k, v| v }
+
+          oxygen_rate << if maximum[1] == minimum[1]
+                           '1'
+                         else
+                           maximum[0]
+                         end
+        else
+          oxygen_rate << oxygen_rate_distribution[i].keys.first.to_s
+        end
+
+        oxygen_rate_report.select! { |line| line[i] == oxygen_rate.last }
+
+        next unless oxygen_rate_report.count == 1
+
+        oxygen_rate = oxygen_rate_report.pop.split('')
+        break
+      end
+
+      (0...input_line_length).each do |i|
+        scrubber_rating_distribution = number_distribution(scrubber_rating_report)
+        if scrubber_rating_distribution[i].count > 1
+          maximum = scrubber_rating_distribution[i].max_by { |_k, v| v }
+          minimum = scrubber_rating_distribution[i].min_by { |_k, v| v }
+
+          scrubber_rating << if maximum[1] == minimum[1]
+                               '0'
+                             else
+                               minimum[0]
+                             end
+        else
+          scrubber_rating << scrubber_rating_distribution[i].keys.first.to_s
+        end
+
+        scrubber_rating_report.select! { |line| line[i] == scrubber_rating.last }
+
+        next unless scrubber_rating_report.count == 1
+
+        scrubber_rating = scrubber_rating_report.pop.split('')
+        break
+      end
+
+      @oxygen_generator_rating = oxygen_rate.join('').to_i(2)
+      @scrubber_rating = scrubber_rating.join('').to_i(2)
+
+      @oxygen_generator_rating * @scrubber_rating
+    end
+
+    private
+
+    def number_distribution(input_array = nil)
+      distribution = []
+      report = input_array || @diagnostic_report
+
+      report.each do |line|
+        index = 0
+        line.each_char do |char|
+          if distribution[index]
+            distribution[index][char] = 0 if distribution[index][char].nil?
+            distribution[index][char] += 1
+          else
+            count = {}
+            count[char] = 1
+            distribution << count
+          end
+          index += 1
+        end
+      end
+      distribution
     end
   end
-
-  d
-end
-# O(n)-> length of the array
-diagnostic_report.each do |line|
-  # O(k) -> size of the hash
-  gamma_rate << line.max_by { |_k, v| v }[0]
-  epsilon_rate << line.min_by { |_k, v| v }[0]
 end
 
-first_most_common = gamma_rate[0]
-
-input_line_length = gamma_rate.join('').size
-oxygen_rate = []
-other_one = []
-
-lines_one = lines.dup
-lines_two = lines.dup
-
-(0...input_line_length).each do |i|
-  t = get_diagnostic_report(lines_one)
-  maximum = t[i].max_by { |_k, v| v }
-  minimum = t[i].min_by { |_k, v| v }
-
-  oxygen_rate << if maximum[1] == minimum[1]
-                   '1'
-                 else
-                   maximum[0]
-                 end
-  lines_one.select! { |line| line[i] == oxygen_rate.last }
-end
-
-(0...input_line_length).each do |i|
-  t = get_diagnostic_report(lines_two)
-
-  maximum = t[i].max_by { |_k, v| v }
-  minimum = t[i].min_by { |_k, v| v }
-
-  other_one << if maximum[1] == minimum[1]
-                 '0'
-               else
-                 minimum[0]
-               end
-  lines_two.select! { |line| line[i] == other_one.last }
-end
-
-# decimal_other_rate = other_one.join('').to_i(2)
-decimal_oxygen_rate = oxygen_rate.join('').to_i(2)
-
-puts decimal_oxygen_rate
-decimal_gamma_rate = gamma_rate.join('').to_i(2)
-decimal_epsilon_rate = epsilon_rate.join('').to_i(2)
-
-puts decimal_gamma_rate * decimal_epsilon_rate
-# puts decimal_oxygen_rate * decimal_other_rate
+binary_diagnostic = ExerciseThree::BinaryDiagnostic.new('./exercise_3_input.txt')
+puts binary_diagnostic.power_consumption
+puts binary_diagnostic.life_support_rating
